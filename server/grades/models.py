808ff48ab2c476a461558c10b24882841bba79d3
Simon Diaz <simon.diaz.abad@gmail.com>
2023-07-09 15:54:06 -0400

    grades are returned as json now

--- server/grades/models.py
+++ server/grades/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Grade(models.Model):
     # this should be a foreign key to a student model
     # we should change this to studentId
-    student = models.IntegerField()
+    studentId = models.IntegerField()
     # term
     term = models.IntegerField(default=0)
     # this should be a foreign key to a course model
     # we should change this to courseId
-    course = models.IntegerField()
+    courseId = models.IntegerField()
     grade = models.FloatField()
 
     def __str__(self):
-        return f"{self.student}, {self.course}, {self.grade}"
+        return f"stutentId: {self.studentId}, term: {self.term}, courseId: {self.courseId}, grade: {self.grade}"
--- server/grades/views.py
+++ server/grades/views.py
@@ -2,6 +2,9 @@
 from django.shortcuts import render
 # import hhtpresponse from django.http
 from django.http import HttpResponse
+from django.core import serializers
+
+from .models import Grade
 
 # Create your views here.
 def index(request):
@@ -9,7 +12,14 @@ def index(request):
 
 # this view returns all grades for a student
 def studentGrades(request, studentId):
-    return HttpResponse(f"grades for student {studentId}")
+    grades = Grade.objects.filter(studentId=studentId)
+    # we return grades as a json object
+    grades_list = serializers.serialize('json', grades)
+    return HttpResponse(grades_list, content_type="text/json-comment-filtered")
+
 
 def studentTermGrades(request, studentId, term):
-    return HttpResponse(f"grades for student {studentId} in term {term}")
+    grades = Grade.objects.filter(studentId=studentId, term=term)
+    # we return grades as a json object 
+    grades_list = serializers.serialize('json', grades)
+    return HttpResponse(grades_list, content_type="text/json-comment-filtered")
