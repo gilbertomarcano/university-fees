b65554f142bbf6043657a9c0aecdca9c8f0b7499
Simon Diaz <simon.diaz.abad@gmail.com>
2023-07-09 17:28:49 -0400

    grade pdf report is being generated now

--- server/grades/urls.py
+++ server/grades/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:student_id>", views.student_grades, name="student_grades"),
-    path("<int:student_id>/<int:term>", views.student_term_grades, name="student_term_grades")
+    path("<int:student_id>/<int:term>", views.student_term_grades, name="student_term_grades"),
+    path("general-report/<int:student_id>", views.student_general_report, name="student_general_report")
+
 ]
--- server/grades/views.py
+++ server/grades/views.py
@@ -1,8 +1,15 @@
 # from http.client import HTTPResponse
+import html
+from multiprocessing import context
+from os import name
+from unittest import result
 from django.shortcuts import render
 # import hhtpresponse from django.http
 from django.http import HttpResponse
 from django.core import serializers
+from io import BytesIO
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 from .models import Grade
 
@@ -23,3 +30,38 @@ def student_term_grades(request, student_id, term):
     # we return grades as a json object 
     grades_list = serializers.serialize('json', grades)
     return HttpResponse(grades_list, content_type="text/json-comment-filtered")
+
+def student_general_report(request, student_id):
+    terms = {}
+
+    grades = Grade.objects.filter(student_id=student_id)
+
+    for course in grades:
+        if course.term not in terms:
+            terms[course.term] = [course]
+        else:
+            terms[course.term].append(course)
+
+    context = {
+        "terms": terms, 
+        "student": {
+            "name": "Simón Díaz", #temporal name, we need a student model
+            "id": student_id
+        },
+    }
+    
+    pdf = render_to_pdf("grades_template.html", context)
+    res = HttpResponse(pdf.getvalue(), content_type="application/pdf")
+    res["Content-Disposition"] = \
+        f"attachment; filename=grades-report-{student_id}.pdf"
+    return res
+
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+
