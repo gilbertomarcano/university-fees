d2808e46d94e4cf9cf0c64fa3ec93387550a22ea
Simon Diaz <simon.diaz.abad@gmail.com>
2023-07-09 21:09:13 -0400

    pdf filed is updated with new models

--- server/grades/views.py
+++ server/grades/views.py
@@ -12,6 +12,8 @@ from django.template.loader import get_template
 from xhtml2pdf import pisa
 
 from .models import Grade
+from students.models import Student, CareerChoices, VenezuelanRegionChoices
+from users.models import User
 
 # Create your views here.
 def index(request):
@@ -33,8 +35,17 @@ def student_term_grades(request, student_id, term):
 
 def student_general_report(request, student_id):
     terms = {}
+    # we get national id prefix and number from student_id
+    student_id_str = str(student_id)
+    id_prefix = student_id_str[0]
+    id_number = student_id_str[1:]
+    print(id_prefix, id_number)
 
-    grades = Grade.objects.filter(student_id=student_id)
+    student = Student.objects.get(national_id_prefix=id_prefix,national_id_number=int(id_number))
+    print(student)
+    grades = Grade.objects.filter(student_id=id_number)
+    user = User.objects.get(id=student.user_id)
+    print(user)
 
     for course in grades:
         if course.term not in terms:
@@ -42,13 +53,32 @@ def student_general_report(request, student_id):
         else:
             terms[course.term].append(course)
 
+    # we convert student career to their name on CareerChoices, using integers as keys
+    # we find the dict that contains student.career
+    student_career = ""
+    for career in CareerChoices.choices:
+        if career[0] == student.career:
+            student.career = career[1]
+            break
+
+    region = ""
+    # we do the same with student.region
+    for region in VenezuelanRegionChoices.choices:
+        if region[0] == student.region:
+            student.region = region[1]
+            break
+
+
+    print(student_career)
+
+
     context = {
         "terms": terms, 
-        "student": {
-            "name": "Simón Díaz", #temporal name, we need a student model
-            "id": student_id
-        },
+        "student": student,
+        "user": user,
     }
+
+    # return render(request, "grades_template.html", context)
     
     pdf = render_to_pdf("grades_template.html", context)
     res = HttpResponse(pdf.getvalue(), content_type="application/pdf")
